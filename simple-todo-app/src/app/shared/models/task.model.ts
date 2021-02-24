5e3cdda94660195b9df54da92e9988652edfde9d
Herco Bezuidenhout <herco@thinwood.co>
2021-02-24 22:45:58 +0200

    connected to firestore

--- simple-todo-app/src/app/shared/models/task.model.ts
+++ simple-todo-app/src/app/shared/models/task.model.ts
@@ -1,5 +1,5 @@
 export interface Task {
-    id: number;
+    id: string;
     task: string;
     dateCreated: Date;
     dateDue?: Date;
--- simple-todo-app/src/app/shared/services/todo/todo.service.ts
+++ simple-todo-app/src/app/shared/services/todo/todo.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Task } from 'src/app/shared/models/task.model';
 
 @Injectable({
@@ -8,37 +9,23 @@ export class TodoService {
 
   tasks: Task[];
 
-  constructor() {
-    this.tasks = JSON.parse(localStorage.getItem('tasks'));
+  constructor(private fire: AngularFirestore) {
+    // this.tasks = JSON.parse(localStorage.getItem('tasks'));
+    this.fire.collection('tasks').valueChanges({ idField: 'id' }).subscribe((tasks: Task[]) => {
+      this.tasks = tasks;
+    });
   }
 
   add(task: Task) {
-    if (!this.tasks) this.tasks = []
-    task.id = this.tasks.length + 1;
-    task.dateCreated = new Date();
-    this.tasks.push(task);
-    this.save();
+    this.fire.collection('tasks').add(task);
   }
 
   update(task: Task) {
-    const position = task.id - 1;
-    this.tasks[position] = task;
-    this.save();
+    this.fire.collection('tasks').doc(task.id).set(task);
   }
 
   remove(task: Task) {
-    this.tasks.splice(this.tasks.indexOf(task), 1);
-    this.updateIds();
-    this.save();
+    this.fire.collection('tasks').doc(task.id).delete();
   }
 
-  private save() {
-    localStorage.setItem('tasks', JSON.stringify(this.tasks));
-  }
-
-  private updateIds() {
-    for (let i = 0; i < this.tasks.length; i++) {
-      this.tasks[i].id = i + 1;
-    }
-  }
 }
