7ef9b60b4cd436cfaa07992d2235b560a4936424
Herco Bezuidenhout <herco@thinwood.co>
2021-02-24 22:36:44 +0200

    imported AngularFireModule & initialized app

--- simple-todo-app/src/app/shared/shared.module.ts
+++ simple-todo-app/src/app/shared/shared.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     FormsModule,
-    MaterialModule
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   exports: [
     CommonModule,
